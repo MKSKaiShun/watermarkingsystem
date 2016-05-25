c799d9a710a1ed0dbf4d4bc456c300d44b6650de
MKSKaiShun <ben10_ks@hotmail.com>
2016-05-25 22:40:35 +0800

    My changes

--- Dbconnect.php
+++ Dbconnect.php
@@ -1,5 +1,5 @@
 <?php
 
-mysqli_connect("127.4.231.130","adminHsiHP3R","av7i3VLdViH1") or die("error");
-mysqli_select_db("watermarkingimark") or die("error");
+mysqli_connect("127.9.144.130","adminw3Lek5K","5D6Nc3xHj_-S") or die("error");
+mysqli_select_db("imarkwatermark") or die("error");
 ?>
