56c46cc8e1f1edbcd01a426e7fd7818058850096
MKSKaiShun <ben10_ks@hotmail.com>
2016-05-25 21:42:38 +0800

    My changes3

--- Dbconnect.php
+++ Dbconnect.php
@@ -1,4 +1,4 @@
 <?php
 
-$con = mysqli_connect('127.13.35.130','admin8nWg8HU','eYbE4_Ef1qiR','imark') or die('error');
+$con = mysqli_connect("127.13.35.130","admin8nWg8HU","eYbE4_Ef1qiR","imark") or die("error");
 ?>
--- Library.php
+++ Library.php
@@ -5,7 +5,7 @@ session_start();
 <?php
 
 	 
-	 $con = mysqli_connect('127.13.35.130','admin8nWg8HU','eYbE4_Ef1qiR','imark') or die('error');
+	 $con = mysqli_connect("127.13.35.130","admin8nWg8HU","eYbE4_Ef1qiR","imark") or die("error");
 	  
 	$sql="SELECT * from image";
 	$result=mysqli_query($con,$sql);
